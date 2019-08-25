c35fe8fbdacc251ca1994746397d4eaf6047efb0
Neeraj Verma <neeraj.viman@gmail.com>
2019-08-25 21:35:11 +0530

    replaces os.FindProcess with ps.FindProcess

--- main.go
+++ main.go
@@ -10,16 +10,20 @@ func exitWithMessage(message string) {
 	os.Exit(1)
 }
 
+func startLogic() {
+	JustKeepLooking(func(gotTheGame Game) {
+		fmt.Println("So you are playing", gotTheGame.Name)
+		fmt.Println("I am tracking you now", gotTheGame.ProcessId)
+		JustKeepWatching(gotTheGame.ProcessId, func(statusChange string) {
+			fmt.Println(statusChange, "now what? Let's start again?")
+			startLogic()
+		})
+	})
+}
+
 func main() {
 
-	//
-	// foundGame := false
-	JustKeepWatching(18864)
-	// JustKeepLooking(func(gotTheGame Game) {
-	// 	fmt.Println("So you are playing", gotTheGame.Name)
-	// 	fmt.Println("I am tracking you now", gotTheGame.ProcessId)
-	// 	JustKeepWatching(gotTheGame.ProcessId)
-	// })
+	//startLogic()
 	for {
 	}
 }
--- process_manager.go
+++ process_manager.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
+	ps "github.com/keybase/go-ps"
 	"time"
+)
+
+type CallbackOnStatusChange func(newStatus string)
+
+const (
+	PROCESS_STATUS_ACTIVE   = "ACTIVE"
+	PROCESS_STATUS_INACTIVE = "INACTIVE" // Minimized
+	PROCESS_STATUS_EXITED   = "EXITED"
 )
 
 var (
@@ -15,18 +23,17 @@ const (
 )
 
 func GetProcessStatus(processId int) (string, error) {
-	process, err := os.FindProcess(processId)
+	yoProcess, err := ps.FindProcess(processId)
 
 	if err == nil {
-		fmt.Print(true)
-		fmt.Printf("Process %d is found", process.Pid)
+		if yoProcess == nil {
+			return PROCESS_STATUS_EXITED, nil
+		} else {
+			return PROCESS_STATUS_ACTIVE, nil
+		}
 	} else {
-		fmt.Println(false)
+		return "ERROR", err
 	}
-
-	fmt.Println(process)
-
-	return "Watching", nil
 }
 
 // This function watches the processId
@@ -34,19 +41,27 @@ func GetProcessStatus(processId int) (string, error) {
 // Supports 2 states:
 // Exited: Process was killed
 // Minimized: The window of the process has been minimized
-func JustKeepWatching(processId int) {
+func JustKeepWatching(processId int, callback CallbackOnStatusChange) {
 
 	// 1. Start ticker. And just keep watching the process
 	tickerForWatcher = time.NewTicker(PROCESS_WATCHER_INTERVAL)
 	fmt.Println("Watcher started.")
 	go func() {
 		for _ = range tickerForWatcher.C {
-			_, err := GetProcessStatus(processId)
+			procStatus, err := GetProcessStatus(processId)
 
 			if err != nil {
 				fmt.Println("Error: ", err)
 			} else {
-				fmt.Println("still looking", processId)
+				switch procStatus {
+				case PROCESS_STATUS_ACTIVE:
+					fmt.Println("Still active")
+				case PROCESS_STATUS_EXITED:
+					fmt.Println("Exited")
+					tickerForWatcher.Stop()
+					callback(procStatus)
+
+				}
 			}
 		}
 	}()
